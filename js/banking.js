510025de146fe3b40165ccad311f6a8bbb87e7ef
Shazzadul-Shakib <shakib1186@gmail.com>
2022-02-12 00:08:03 +0600

    Finally done

--- js/banking.js
+++ js/banking.js
@@ -1,39 +1,77 @@
 
+// //! value of input Function
+function inputFields(deposit) {
+  const inputField = document.getElementById(deposit);
+  const newInput = inputField.value;
+  //! Clear  input
+  inputField.value = '';
+  if (newInput > 0) {
+    return newInput;
+  }
+  else {
+    return 0;
+  }
+    
+};
+
+function getCurrentBalance() {
+  const currentTotal = document.getElementById('previous-total').innerText;
+  console.log(parseFloat(currentTotal));
+  return parseFloat(currentTotal);
+  
+}
+
+//! Value of Deposit and withdraw ammount Function 
+function summery(prevData, newInputedData) {
+  const previousDeposite = document.getElementById(prevData);
+ 
+  if (newInputedData > 0) {
+    const finalDeposite=  parseFloat(previousDeposite.innerText)+parseFloat(newInputedData);
+   previousDeposite.innerText = finalDeposite;
+  }
+  else {
+    alert("WRONG INPUT!");
+  }
+};
+// ! final total Function
+function total(prevTotal,newInputedData,isAdd) {
+  const previousTotal = document.getElementById(prevTotal);
+  if (isAdd == true) {
+    const finalTotal = parseFloat(previousTotal.innerText) + parseFloat(newInputedData);
+    previousTotal.innerText = finalTotal;
+  }
+  else {
+    if (newInputedData > parseFloat(previousTotal.innerText)) {
+      alert("Insufficient Balance");
+    }
+    else {
+      const finalTotal = parseFloat(previousTotal.innerText) - parseFloat(newInputedData);
+      
+      previousTotal.innerText = finalTotal;
+
+    }
+  }
+      
+};
 // ! deposite event handler
 document.getElementById('deposite-button').addEventListener('click', function () {
-  //! value of input
-  const depositeField = document.getElementById('deposite-input');
-  const newDeposite = depositeField.value;
-  // ! Deposited ammount section
-  const previousDeposite = document.getElementById('previous-deposited');
-  const finalDeposite=  parseFloat(previousDeposite.innerText)+parseFloat(newDeposite);
-  // ! deposite add to Total amount
-  const previousTotal = document.getElementById('previous-total');
-  const finalTotal = parseFloat(previousTotal.innerText) + parseFloat(newDeposite);
- //! value passed to Deposite section
-  previousDeposite.innerText = finalDeposite;
-  //! Final value to total
-  previousTotal.innerText = finalTotal;
-  // ! Clear deposite input
-  depositeField.value = '';
+// //! value of input
+const newDeposite = inputFields('deposite-input');
+// ! Deposited ammount section
+const finalDeposite = summery('previous-deposited', newDeposite);
+// ! final total
+const finalTotal = total('previous-total', newDeposite,true);
 });
-// ! deposite event handler
+// ! withdraw event handler
 document.getElementById('withdraw-button').addEventListener('click', function () {
-  // ! Withdraw input
-  const withdrawField = document.getElementById('withdraw-input');
-  const newWithdraw = withdrawField.value;
-  // ! Withdraw ammount section
-  const previousWithdraw = document.getElementById('previous-Withdrew');
-  const finalWithdraw = parseFloat(previousWithdraw.innerText) + parseFloat(newWithdraw);
-// ! withdraw substruction to total
-  const previousTotal = document.getElementById('previous-total');
-  const finalTotal = parseFloat(previousTotal.innerText) - parseFloat(newWithdraw);
- //! value passed to Withdraw section
-  previousWithdraw.innerText = finalWithdraw;
-  // ! Final ammount after withdraw
-  previousTotal.innerText = finalTotal;
-  // ! Clear withdraw input
-  withdrawField.value = '';  
+// ! Withdraw input
+  const newWithdraw = inputFields('withdraw-input');
+// ! Withdraw ammount section
+  if (getCurrentBalance() > newWithdraw) {
+     const finalWithdraw = summery('previous-Withdrew', newWithdraw);
+  }
+// !final total
+  const finalTotal = total('previous-total', newWithdraw, false); 
 });
 //! Return to login PAGE
 document.getElementById('return-button').addEventListener('click', function () {
